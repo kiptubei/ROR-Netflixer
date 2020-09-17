f56808a5d05148e8477c10ead9d12eab656dfce4
kiptubei <kiptubei@gmail.com>
2020-09-17 09:35:57 +0300

    Modified  db_schema to add likes functionality

--- db/migrate/20200914172744_add_foreign_key_to_opinions2.rb
+++
@@ -1,6 +0,0 @@
-class AddForeignKeyToOpinions2 < ActiveRecord::Migration[6.0]
-  def change
-    change_column :opinions, :authorid, 'bigint USING CAST(authorid AS bigint)'
-    add_foreign_key :opinions, :users, column: :authorid
-  end
-end
