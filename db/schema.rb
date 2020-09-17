a960d430dbc244067192504edb313ed0f57023eb
kiptubei <kiptubei@gmail.com>
2020-09-17 09:55:12 +0300

    Dropped table likes and removed buggy like implementation

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 2020_09_17_190919) do
+ActiveRecord::Schema.define(version: 2020_09_17_190920) do
 
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
@@ -18,13 +18,6 @@ ActiveRecord::Schema.define(version: 2020_09_17_190919) do
   create_table "followings", force: :cascade do |t|
     t.integer "followerId"
     t.integer "followedId"
-    t.datetime "created_at", null: false
-    t.datetime "updated_at", null: false
-  end
-
-  create_table "likes", force: :cascade do |t|
-    t.integer "OpinionId"
-    t.integer "UserId"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
   end
