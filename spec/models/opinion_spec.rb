114abb35e264ebd5e6abe2c08a33fe529d05f887
kiptubei <kiptubei@gmail.com>
2020-09-17 12:46:10 +0300

    Added tests for opinion model validations and associations

---
+++ spec/models/opinion_spec.rb
@@ -0,0 +1,12 @@
+require 'rails_helper'
+
+RSpec.describe Opinion, type: :model do
+  describe 'validations' do
+    it { should validate_presence_of(:Text) }
+    it { should_not allow_value(' ').for(:Text) }
+  end
+
+  describe 'associations' do
+    it { should belong_to(:Author).with_foreign_key(:AuthorId).class_name('User') }
+  end
+end
