a9ffe7032490a94749c63678ae81c00441a9a008
kiptubei <kiptubei@gmail.com>
2020-09-17 13:25:17 +0300

    Added capybara test for creating an opinion

---
+++ spec/features/opinion.spec.rb
@@ -0,0 +1,18 @@
+require 'rails_helper'
+
+RSpec.feature 'Opinion', type: :feature do
+  before :each do
+    # @user = User.create(Fullname: 'Ben Ten', Username: 'ten')
+    @user = User.create(Fullname: 'Django', Username: 'django')
+    Opinion.create(AuthorId: @user.id, Text: '@The_usual_suspects is the one!')
+  end
+
+  it 'Should create an opinion' do
+    visit root_path
+    fill_in 'Username', with: 'django'
+    click_button 'Log In'
+    fill_in 'opinion[Text]', with: '@The_usual_suspects is the one!'
+    click_button 'Speak!'
+    expect(page).to have_content('HOME')
+  end
+end
