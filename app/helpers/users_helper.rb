cab507f2cac1350c203773ec7a91782a0ea4ddea
kiptubei <kiptubei@gmail.com>
2020-09-17 16:16:11 +0300

    Corrected Follow and Unfollow capybara tests

--- app/helpers/users_helper.rb
+++ app/helpers/users_helper.rb
@@ -1,7 +1,7 @@
 module UsersHelper
   def render_user(user)
     if user[:Photo].nil?
-      image_tag(opinion.Author.Photo.thumb.url)
+      # image_tag(opinion.Author.Photo.thumb.url)
       gravatar_for(user, size: 50)
     else
       image_tag(user.Photo.thumb.url, class: 'photo')
@@ -10,7 +10,7 @@ module UsersHelper
 
   def user_img(_user)
     if @user[:Photo].nil?
-      image_tag(@opinion.Author.Photo.thumb.url)
+      # image_tag(@opinion.Author.Photo.thumb.url)
       gravatar_for(@user, size: 100)
     else
       image_tag(@user.Photo.medium.url, class: 'rounded-circle ml-3 mr-3')
