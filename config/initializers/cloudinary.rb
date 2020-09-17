e31e1551ce9f38ebb3d3495cb88b21c5f3b56026
kiptubei <kiptubei@gmail.com>
2020-09-17 09:26:16 +0300

    Changed cloudinary and db configs

--- config/initializers/cloudinary.rb
+++ config/initializers/cloudinary.rb
@@ -1,5 +1,9 @@
 Cloudinary.config do |config|
-  config.cloud_name = ENV['cloudinary_cloud']
-  config.api_key = ENV['cloudinary_key']
-  config.api_secret = ENV['cloudinary_secret']
+  # config.cloud_name = ENV['cloudinary_cloud']
+  # config.api_key = ENV['cloudinary_key']
+  # config.api_secret = ENV['cloudinary_secret']
+
+  config.cloud_name = 'dpjcv6qnt'
+  config.api_key = '444115574563926'
+  config.api_secret = 'SBWTcTXIqyxWLki-33ABNH-UgVo'
 end
